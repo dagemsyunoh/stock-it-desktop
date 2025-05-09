3f1bc2a8562c99fafd231485eaac3255ad56c13a
Lock <81725500+dagemsyunoh@users.noreply.github.com>
2025-05-10 03:02:31 +0800

    base
    Signed-off-by: Lock <jgfranciaph@gmail.com>

---
+++ src/main/java/com/lock/stockit/HelloController.java
@@ -0,0 +1,14 @@
+package com.lock.stockit;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("I am James");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.lock.stockit {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.lock.stockit to javafx.fxml;
+    exports com.lock.stockit;
+}
